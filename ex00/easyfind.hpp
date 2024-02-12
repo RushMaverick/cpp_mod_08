1ce61c96a8ae304bd33de55b64a05ba35417c0be
Rasmus Rask <rasse.rask@gmail.com>
2024-02-12 14:52:56 +0200

    started work on 08, finished 00

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,16 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <vector>
+#include <exception>
+
+template <typename T>
+int easyfind(T &bucket, int n){
+	typename T::iterator it = std::find(bucket.begin(), bucket.end(), n);
+	if (it == bucket.end())
+		throw std::exception();
+	return *it;
+}
+
+#endif
